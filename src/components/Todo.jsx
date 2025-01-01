216428fc018bca373474d9e272b9ff3dc58296de
banushasarma <sharmabanu03@gmail.com>
2025-01-01 13:51:13 +0530

    Todo items add function completed.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
+import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") return null;
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+    console.log(todoList);
+  };
+
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/* -------Title----- */}
@@ -12,17 +31,32 @@ const Todo = () => {
       {/* -------input box----- */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           type="text"
           placeholder="Add your task"
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-w ite text-lg font-medium cursor-pointer"
+        >
           ADD +
         </button>
       </div>
 
       {/* -------Task list----- */}
-      <div></div>
+      <div>
+        {todoList.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              text={item.text}
+              id={item.id}
+              isComplete={item.isComplete}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
