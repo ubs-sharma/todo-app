817c6db5ab786e851ae2a801184a1f3edf2f349f
banushasarma <sharmabanu03@gmail.com>
2025-02-19 21:40:56 +0530

    Add extension

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
-import TodoItems from "./TodoItems";
+import TodoItems from "./TodoItems.jsx";
 
 const Todo = () => {
   const [todoList, setTodoList] = useState(
